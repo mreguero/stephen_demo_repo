1155df9417a09a36463e1aad7df251fe961db36e
Miguel Reguero Bejar <miguel.reguero@i4s.com>
2015-11-29 13:14:31 +0100

    Added tests for feature a

---
+++ test_7.py
@@ -0,0 +1,12 @@
+@given(u'a')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given a')
+
+@when(u'a')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When a')
+
+@then(u'a')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then a')
+
