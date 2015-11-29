8070023f6541f09136fdb18d696b1ab2bafe8b7e
Miguel Reguero <mreguerob0@gmail.com>
2015-11-29 13:58:58 -0600

    Added tests for feature Como usuario quiero poder seleccionar el idioma en el que aparecen los campos para ser multidioma

---
+++ test_11.py
@@ -0,0 +1,12 @@
+@given(u'el usuario en la página')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given el usuario en la página')
+
+@when(u'hace click sobre la bandera')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When hace click sobre la bandera')
+
+@then(u'los textos se cambian a ese lenguaje')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then los textos se cambian a ese lenguaje')
+
