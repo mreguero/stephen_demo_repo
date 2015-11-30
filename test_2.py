447cb0f26a356cb6322e927aef0bc6794f7fec34
Miguel Reguero <miguel.reguero@innovation4security.com>
2015-11-30 03:22:51 -0600

    Added tests for feature Description

---
+++ test_2.py
@@ -0,0 +1,12 @@
+@given(u'un boton')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given un boton')
+
+@when(u'lo pulsas')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When lo pulsas')
+
+@then(u'hace cosas')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then hace cosas')
+
